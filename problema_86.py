e69e588c15806534c0f936416fb52b776e26656f
caiocamposz <caioow.novo@gmail.com>
2026-03-26 11:42:40 -0300

    Create problema_86.py

---
+++ problema_86.py
@@ -0,0 +1,18 @@
+linha_0=[]
+linha_1=[]
+linha_2=[]
+
+for c in range (0,3):
+    num0=int(input("Digite o numero para a posicao (0,{}) : ".format(c)))
+    linha_0.append(num0)
+for c in range (0,3):
+    num1=int(input("Digite o numero para a posicao (1,{}) : ".format(c)))
+    linha_1.append(num1)
+for c in range (0,3):
+    num2=int(input('Digite o numero para a posicao (2,{}) : '.format(c)))
+    linha_2.append(num2)
+
+c=0
+print('[ {} ] [ {} ] [ {} ]'.format(linha_0[c],linha_0[c+1],linha_0[c+2]))
+print('[ {} ] [ {} ] [ {} ]'.format(linha_1[c],linha_1[c+1],linha_1[c+2]))
+print('[ {} ] [ {} ] [ {} ]'.format(linha_2[c],linha_2[c+1],linha_2[c+2]))
